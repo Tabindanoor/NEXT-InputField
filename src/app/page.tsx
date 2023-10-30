881e9355fd715042f61f4b4d55cc50df43646124
TabindaNoor <tabindanoor415@gmail.com>
2023-10-30 23:56:20 +0500

    mesage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,13 @@
-"use client"
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import React from 'react'
 
-export default function Home() {
-  const router = useRouter();
-  const [name, setName] = useState('')
-
-  const handleChange=(e:any)=>{
-    setName(e.target.value)
-    console.log(e.target.value)
-  }
-  const handleSubmit=(e:any)=>{
-    e.preventDefault();
-    router.push(`/prediction/${name}`)
-
-  }
+const page = () => {
   return (
-   <div className='items-center'>
-      <p className='bg-green-400 text-center p-3'>My name is Tabinda Noor</p>
-      
-        <br />
-      <div>
-      <form className='text-center ' onSubmit={(e)=>handleSubmit(e)}>
+    <div>
+      this is me tabinda nooor
 
-          <input type="text" 
-                  onChange={(e)=>handleChange(e)} 
-                 value={name} 
-                 placeholder='enter your name here ' 
-                 className='p-3 bg-slate-300 rounded-lg text-gray-700'  />
-
-
-          <button className='text-center justify-center p-3 bg-purple-200 rounded-lg text-green-700 ' type="submit">Predict</button>   
-      </form>
-      </div>
-      
-   </div>
+    <h1>this is also me tabinda noor</h1>
+    </div>
   )
 }
+
+export default page
