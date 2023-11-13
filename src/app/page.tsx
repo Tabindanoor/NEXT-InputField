23d195364fa93da9de3b6abce3ca93fc7175ef72
TabindaNoor <tabindanoor415@gmail.com>
2023-11-13 17:42:18 +0500

    confusion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,39 @@
+import TodoItem from '@/components/TodoItem'
 import { prisma } from '@/db'
 import { Prisma } from '@prisma/client'
+import { strict } from 'assert'
 import Image from 'next/image'
 import Link from 'next/link'
 import { title } from 'process'
 
 export default async function  Home() {
-  const todos = await prisma.todo.create({data: {title: 'test',complete:false}})
-  // const todos = await prisma.todo.findMany()
-  // console.log(todos,"hehe")
-  // console.log(todos,"hehe")
-  // console.log(todos,"hehe")
+  // const todos = await prisma.todo.create({data: {title: 'test',complete:false}})
+  const todoos = await prisma.todo.findMany()
+ 
+  const handleChange=async(id:string,complete:boolean)=>{ 
+    "use server "
+    console.log(id,complete)
+    prisma.todo.update({where: {id},data: {complete}})
+  }
   return (
   <div className='text-center text-white p-4'>
    <div className='justify-between text-center flex '>
     <p className='text-2xl'>All todos</p>
-    <Link  href="/new" className='p-2  border-slate-400 border rounded-2xl text-xl'>New</Link>
+    <Link  href="/new" className='p-2  border-slate-400 border rounded-2xl text-xl'>
+      New
+    </Link>
+    
    </div>
+   {/* {JSON.stringify(todoos)} */}
+    <div> 
+      {todoos.map((todo)=>{
+        return(
+            <TodoItem key={todo.id} {...todo} handleChange={handleChange} />
+        )
+    })}
+
+    </div>
    </div>
 
-
-  
-  </div>
   )
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+"use client"
 
-const TodoItem = () => {
+type TodoTypes ={
+  id: string
+  complete: boolean
+  title: string
+  handleChange: (id:string,complete:boolean) => void
+}
+
+const TodoItem = ({id,complete,title,handleChange}:TodoTypes) => {
   return (
-    <div>TodoItem</div>
+    <div className='flex '>
+      <input 
+      type="checkbox" 
+      name="title" 
+      id={id} 
+      defaultChecked={complete}
+      className='peer cursor-pointer'  
+      onChange={e=>handleChange(id,e.target.checked)} />
+
+      <label htmlFor={id} className='peer-checked:line-through peer-checked:text-gray-500 cursor-pointer'>
+         {title}
+      </label>
+      
+    </div>
   )
 }
 
