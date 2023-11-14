eec4548d43ee52a35ed2ee681e535fcd83f132f6
TabindaNoor <tabindanoor415@gmail.com>
2023-11-14 22:52:02 +0500

    confusion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,24 @@
-import TodoItem from '@/components/TodoItem'
-import { prisma } from '@/db'
-import { Prisma } from '@prisma/client'
-import { strict } from 'assert'
-import Image from 'next/image'
-import Link from 'next/link'
-import { title } from 'process'
+import { prisma } from "@/db"
 
-export default async function  Home() {
-  // const todos = await prisma.todo.create({data: {title: 'test',complete:false}})
-  const todoos = await prisma.todo.findMany()
- 
-  const handleChange=async(id:string,complete:boolean)=>{ 
-    "use server "
-    console.log(id,complete)
-    prisma.todo.update({where: {id},data: {complete}})
-  }
+
+const page = () => {
+  const mydata = prisma.todo.create({data})
+  // const myTitle = prisma.todo.findMany()
+
   return (
-  <div className='text-center text-white p-4'>
-   <div className='justify-between text-center flex '>
-    <p className='text-2xl'>All todos</p>
-    <Link  href="/new" className='p-2  border-slate-400 border rounded-2xl text-xl'>
-      New
-    </Link>
-    
-   </div>
-   {/* {JSON.stringify(todoos)} */}
-    <div> 
-      {todoos.map((todo)=>{
-        return(
-            <TodoItem key={todo.id} {...todo} handleChange={handleChange} />
-        )
-    })}
-
+    <div className="text-white">
+      <div className="flex  justify-between p-2">
+        <h1 className="title">Welcome to the React App!</h1>
+        <button type="button"  className="border rounded-lg border-gray-300 p-2">Button</button>
+      </div>
+      <h1>Here&apos;s my todo list </h1>
+      {/* {
+        todos.map((todo)=>{
+          <li>{title}</li>
+        })
+      } */}
     </div>
-   </div>
-
   )
 }
+
+export default page
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,28 +1,8 @@
-"use client"
 
-type TodoTypes ={
-  id: string
-  complete: boolean
-  title: string
-  handleChange: (id:string,complete:boolean) => void
-}
 
-const TodoItem = ({id,complete,title,handleChange}:TodoTypes) => {
+const TodoItem = () => {
   return (
-    <div className='flex '>
-      <input 
-      type="checkbox" 
-      name="title" 
-      id={id} 
-      defaultChecked={complete}
-      className='peer cursor-pointer'  
-      onChange={e=>handleChange(id,e.target.checked)} />
-
-      <label htmlFor={id} className='peer-checked:line-through peer-checked:text-gray-500 cursor-pointer'>
-         {title}
-      </label>
-      
-    </div>
+    <div>TodoItem</div>
   )
 }
 
