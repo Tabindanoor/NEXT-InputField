e3272b7b714baa2e49af5a659b09237c9602d0aa
TabindaNoor <tabindanoor415@gmail.com>
2023-11-15 22:20:46 +0500

    dealing with prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 import { prisma } from "@/db"
 
+// const FindTodo=async()=>{
+//   await prisma.todo.findMany()
+// }
 
-const page = () => {
-  const mydata = prisma.todo.create({data})
-  // const myTitle = prisma.todo.findMany()
+const page =async () => {
+//  prisma.todo.create({data:{:"bwebfbegb",complete:false}})
+//  const myTitle =  await prisma.todo.findMany() 
+
+  // const myTitle = FindTodo();
+  // console.log(myTitle,"hehe")
 
   return (
     <div className="text-white">
+
+
+
       <div className="flex  justify-between p-2">
         <h1 className="title">Welcome to the React App!</h1>
         <button type="button"  className="border rounded-lg border-gray-300 p-2">Button</button>
       </div>
       <h1>Here&apos;s my todo list </h1>
+      {/* {JSON.stringify(myTitle)} */}
       {/* {
-        todos.map((todo)=>{
+        myTitle.map((todo)=>{
           <li>{title}</li>
         })
       } */}
